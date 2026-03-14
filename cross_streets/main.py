bd2153efde8dd01ad6678e40163bd450d7b894a2
e11evin-11 <lf.shubrahim@gmail.com>
2026-03-14 18:27:21 +0000

    Make overpass query readable

--- cross_streets/main.py
+++ cross_streets/main.py
@@ -17,14 +17,25 @@ class CrossStreets:
         self.overpass = Overpass()
         self.searchArea = searchArea
 
-    def geocode(self, raw_intersection:str)->Location:
-        street_1, street_2 = raw_intersection.split(' & ')
+    def geocode(self, raw:str)->Location|None:
+        street_1, street_2 = raw.split(' & ') if '&' in raw else raw.split(' and ')
         return self._geocode_clean_intersection(street_1, street_2)
     
-    def _geocode_clean_intersection(self, street_1:str, street_2):
-        query = f'{self.searchArea}->.searchArea;way["highway"]["name"="Hennepin Avenue"](area.searchArea)->.w1;.searchArea;way["highway"]["name"="South 1st Street"](area.searchArea)->.w2; node(w.w1)(w.w2)->.intersection;(.intersection;);out body;'
-        node = self.overpass.query(query).nodes()[0]
-        return Location(latitude=node.lat(), longitude=node.lon())
+    def _geocode_clean_intersection(self, street_1:str, street_2)->Location|None:
+        query = f"""
+            {self.searchArea}->.searchArea;
+            way["highway"]["name"="{street_1}"](area.searchArea)->.w1;
+            way["highway"]["name"="{street_2}"](area.searchArea)->.w2; 
+            node(w.w1)(w.w2)->.intersection;
+            (.intersection;);
+            out body;
+        """
+        results = self.overpass.query(query)
+        if results is None:
+            return None
+        else:
+            node = results.nodes()[0]
+            return Location(latitude=node.lat(), longitude=node.lon())
 
 if __name__ == "__main__":
     cs = CrossStreets()
