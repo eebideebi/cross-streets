53ab589f8a2b491503b2d6d82704a40f54b59027
e11evin-11 <lf.shubrahim@gmail.com>
2026-03-14 18:03:15 +0000

    completed MVP

--- cross_streets/main.py
+++ cross_streets/main.py
@@ -1,15 +1,31 @@
 from pydantic import BaseModel
+from OSMPythonTools.overpass import Overpass
+
+# --- Types ---
 
 class Location(BaseModel):
     latitude: float
     longitude: float
 
+# --- Main ---
+
 class CrossStreets:
-    def __init__(self):
-        print("running init")
+    def __init__(
+        self,
+        searchArea:str = 'area(3600136712)' # geocodeArea:Minneapolis
+    ):
+        self.overpass = Overpass()
+        self.searchArea = searchArea
+
     def geocode(self, raw_intersection:str)->Location:
-        return Location(latitude=1, longitude=1)
+        street_1, street_2 = raw_intersection.split(' & ')
+        return self._geocode_clean_intersection(street_1, street_2)
+    
+    def _geocode_clean_intersection(self, street_1:str, street_2):
+        query = f'{self.searchArea}->.searchArea;way["highway"]["name"="Hennepin Avenue"](area.searchArea)->.w1;.searchArea;way["highway"]["name"="South 1st Street"](area.searchArea)->.w2; node(w.w1)(w.w2)->.intersection;(.intersection;);out body;'
+        node = self.overpass.query(query).nodes()[0]
+        return Location(latitude=node.lat(), longitude=node.lon())
 
 if __name__ == "__main__":
     cs = CrossStreets()
-    print(cs.geocode('test'))
+    print(cs.geocode('Hennepin Avenue & South 1st Street'))
