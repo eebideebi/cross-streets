5e9fd06e9a7671fd882d27f483d842f16f726bf0
eebideebi <eebideebi@proton.me>
2026-03-14 12:09:54 -0500

    added docs to package

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# crosstreets
-Python Library that translates human input into 
+# cross_streets
+Python Library that translates cross streets into Overpass/Nominatim queries 
+
+
+# Building from source
+1. Clone this repository
+2. Install `requirements.txt`
+3. Run `python setup.py sdist bdist_wheel`
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+# Run with `python setup.py sdist bdist_wheel`
+# Publish with `twine upload dist/*`
+
 from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    description = f.read()
 
 setup(
     name='cross_streets',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
       'pydantic >= 2.12'  
-    ]
+    ],
+    long_description=description,
+    long_description_content_type='text/markdown'
 )
